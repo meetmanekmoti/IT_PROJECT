aa1fa87545140664076e91f9976091769f264c17
meetmanekmoti <meet.manekmoti@gmail.com>
2018-04-18 14:18:27 +0530

    lol

--- login.php
+++ login.php
@@ -5,7 +5,8 @@
 		Username:<input type="text" name="name" required><br>
 		Password:<input type="password" name="password" required><br>
 		<input type="submit" value="submit">
-		<?-- Cloned --?>
+		<?-- Cloned and again Cloned --?>
+		
 		</form>
 	</body>
 </html>
